af6f2be4126c3d3bc91a014ab65c7cf0963f0d9c
Anshuman Dubey <anshumandubey1609@gmail.com>
2023-11-21 13:33:13 +0530

    description include

--- src/component/Basics/menuApi.js
+++ src/component/Basics/menuApi.js
@@ -2,39 +2,39 @@ const Menu = [
     {
         id: 1,
         image: "images/maggi.jpg",
-        name: "maggi",
+        name: "Maggi",
         category: "breakfast",
-        price: "12₹",
+        price: "15₹",
         description:
-            "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+            " Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes. ",
     },
 
     {
         id: 2,
         image: "images/allupakoida.jpg",
-        name: "allu pakoida",
+        name: "Alloo pakoda",
         category: "evening",
-        price: "20₹",
+        price: "25₹",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+            "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji .Aloo pakora is a popular street food snack served not only in Mumbai but in other parts of India as well. There was a popular vendor who made the tastiest pakoras near my office. . ",
     },
     {
         id: 3,
         image: "images/corn.jpg",
-        name: "corn",
+        name: "Corn",
         category: "breakfast",
-        price: "10₹",
+        price: "40₹",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+            "This Fried corn is very delicious as well as healthy. It is good for breakfast as it is not so heavy and also provides all nutrients. ",
     },
     {
         id: 4,
         image: "../images/chola.jpg",
-        name: "chola",
+        name: "Chhole",
         category: "lunch",
         price: "50₹",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+            "Chhole, a North Indian culinary delight, features tender chickpeas enveloped in a robust and aromatic tomato-based gravy. Bursting with a symphony of spices, this dish is a vegetarian favorite, best enjoyed with rice or assorted Indian breads.. ",
     },
     {
         id: 5,
@@ -43,7 +43,7 @@ const Menu = [
         category: "evening",
         price: "80₹",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+            "Pizza, an iconic and globally cherished dish, originates from Italian cuisine but has evolved into countless variations worldwide. Characterized by a thin or thick crust topped with tomato sauce, cheese, and various toppings, pizza offers a diverse and customizable dining experience. Whether adorned with classic Margherita simplicity or adorned with creative combinations, pizza remains a universally loved comfort food. ",
     },
     {
         id: 6,
@@ -51,8 +51,7 @@ const Menu = [
         name: "Non-Veg Thali",
         category: "dinner",
         price: "180₹",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        description: "Typically served on a platter, the thali includes a selection of non-vegetarian dishes such as curries, gravies, and meats, accompanied by assorted bread, rice, and condiments. ",
     },
     {
         id: 7,
@@ -61,7 +60,7 @@ const Menu = [
         category: "dinner",
         price: "60₹",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+            "Sweets, also known as desserts or confections, encompass a wide array of delectable treats enjoyed across cultures. Ranging from the rich and creamy to the light and fruity, sweets satisfy the sweet tooth and often hold cultural and celebratory significance. Traditional sweets vary globally, including iconic examples like baklava, tiramisu, gulab jamun, and pastries. ",
     },
     {
         id: 8,
@@ -70,7 +69,7 @@ const Menu = [
         category: "lunch",
         price: "60₹",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+            "Rajma Rice, a beloved North Indian dish, harmonizes the earthy goodness of kidney beans in a spiced tomato-based gravy with fluffy rice. This hearty and flavorful combination, often accompanied by small side dishes, captures the essence of comfort and satisfaction in every bite. ",
     },
     {
         id: 9,
@@ -79,7 +78,7 @@ const Menu = [
         category: "evening",
         price: "10₹",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+            "Samosa, a popular and savory snack originating from South Asia, is a triangular pastry filled with a mixture of spiced potatoes, paneer & peas. This delightful treat is typically deep-fried until golden and crispy, creating a perfect contrast to the flavorful and aromatic filling. Samosas are enjoyed across various cultures and are often served with chutneys or sauces, making them a beloved street food and party favorite.",
     },
 ];
 
